a5a4fdba26f9e8ef3d2bd54d43c0aafd6e9e4173
gwvsol <gwvsol@gmail.com>
2018-11-08 21:18:21 +0200

    Removed the files to check the correct operation of the controller

--- archive_library/i2c_ds3231_test.py
+++
@@ -1,8 +0,0 @@
-from machine import I2C, Pin
-import time
-from i2c_ds3231 import DS3231
-i2c = I2C(scl=Pin(5), sda=Pin(4), freq=400000)
-rtc = DS3231(i2c, 0x68, 3, True, 'ntp')
-rtc.save_time(True)
-rtc.rtctime()
-time.localtime()
